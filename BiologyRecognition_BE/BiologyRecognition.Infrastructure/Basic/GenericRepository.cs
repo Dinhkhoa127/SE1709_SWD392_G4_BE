f5e7b43e660156b571f4c06279cc81aee76d2a31
HuynhNguyenQueTran <tranhnqse183031@fpt.edu.vn>
2025-07-23 15:51:28 +0700

    code_basicGeRepo

--- BiologyRecognition_BE/BiologyRecognition.Infrastructure/Basic/GenericRepository.cs
+++ BiologyRecognition_BE/BiologyRecognition.Infrastructure/Basic/GenericRepository.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BloodDonation.Repositories.NhanNB.Basic
+namespace BiologyRecognition.Infrastructure.Basic
 {
     public class GenericRepository<T> where T : class
     {
